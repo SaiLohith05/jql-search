2613a3a533fe18ce06df781055e3556c69b285aa
SaiLohith05 <96807536+SaiLohith05@users.noreply.github.com>
2025-11-09 21:25:00 +0530

    Initial commit with full React project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [input,setInput]=useState();
+  const [output,setOutput]=useState();
+  const [theme,setTheme]=useState(false);
+  const [showToast, setShowToast] = useState(false);
+  
+
+  const handleSubmit=()=>{
+    const lines =input.split("\n").map((line)=>line.trim()).filter((line)=>line!=="")
+    const ans=lines.map((line)=>'text ~ "' +line+'*" OR');
+    
+    setOutput("issue in ( "+ans.join("\n").slice(0, -3) +");")
+  }
+
+  const handleCopy=()=>{
+     navigator.clipboard.writeText(output)
+    .then(() => {
+     setShowToast(true);
+     setTimeout(()=>setShowToast(false),2000)
+    })
+    .catch((err) => {
+      console.error("Failed to copy: ", err);
+    });
+  }
+
+   const handleCloseToast = () => {
+    setShowToast(false);
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${"container"} ${theme ? "light" : "dark"}`}>
+      <h1>JQL Search</h1>
+      <button onClick={()=>setTheme((prev)=> !prev)}>{theme?"dark":"light"}</button>
+      <label>Paste text</label>
+      <textarea type="text" name="input" value={input} onChange={(e)=>setInput(e.target.value)}>
+      </textarea>
+      <div>
+        <button onClick={handleSubmit}>submit</button>
+        <button onClick={handleCopy}>Copy to clipboard</button>
+      </div>
+      <textarea type="text" name="output" value={output}>
+      </textarea>
+      {showToast && (
+        <div className="toast-overlay">
+          <div className="toast-modal">
+            <p>Copied to clipboard successfully!</p>
+            <button onClick={handleCloseToast}>OK</button>
+          </div>
+        </div>
+      )}
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
